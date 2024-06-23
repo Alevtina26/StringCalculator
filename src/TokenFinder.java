26abf3021a5a781fba562748b86a5da583b7b97b
Alevtina26 <safonova263519@yandex.ru>
2024-06-23 18:29:16 +0300

    First commit

---
+++ src/TokenFinder.java
@@ -0,0 +1,27 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TokenFinder {
+    private String inputString;
+    String[] tokenArray= new String[4];
+
+    public TokenFinder(String inputString) {
+        this.inputString = inputString;
+    }
+
+    public String[] getTokens() throws Exception {
+        //задаем паттерн для поиска
+        Pattern pattern=Pattern.compile("(\".{1,10}\") ([+|*|\\-|/]) (\\d{1,2}|\".{1,10}\")");
+        Matcher matcher=pattern.matcher(inputString);
+        if (matcher.matches()){
+            //если строка в нужном нам формате то сохраняем данные из групп в масив токенов
+            for (int i=1; i<=3; i++) {
+                tokenArray[i]=matcher.group(i);
+            }
+        } else {
+            throw new Exception("Неверный формат данных");
+        }
+        return tokenArray;
+    }
+
+}
